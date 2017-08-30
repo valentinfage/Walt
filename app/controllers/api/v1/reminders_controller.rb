fbd937c23c1f8bc33b2cfbcca7166a22f04774b3
valentin fage <valentin-fage@vfage.com>
2017-08-30 11:16:33 +0200

    pas fini

--- app/controllers/api/v1/reminders_controller.rb
+++ app/controllers/api/v1/reminders_controller.rb
@@ -14,24 +14,51 @@ class Api::V1::RemindersController < Api::V1::BaseController
 
     def update
         if @reminder.update(reminder_params)
-            head :ok
+            render :index
         else
             render_error
         end
     end
 
     def create
+      # Parameters: {"type"=>"reminder", "when"=>"tomorow", "content"=>"pasta"}
+        # @var_content = JSON.parse(params[:content])
+        # @var_date = JSON.parse(params[:when])
+        # @var_action = JSON.parse(params[:type])
+        # render :json => params[:content]
+        # Content-Type: application/JSON
+
+        # @reminder = Reminder.new(reminder_params)
+          # if @reminder.save
+          #     render :json => @reminder,
+          #     :when => true
+          # else
+          #     render :json => @reminder.error
+          #     # render_error
+          # end
+
+        puts "hello"
         @reminder = Reminder.new(reminder_params)
         @reminder.user = current_user
+        puts "hello1"
+        time = check_time(params[:reminder][:when])
+        puts "hello2"
+        # reminder.date = time
+        puts time
+
+
         @reminder.jstime = Time.new(@reminder.date.year, @reminder.date.month,
                                 @reminder.date.day, @reminder.time.hour,
                                 @reminder.time.min).to_i * 1000
         authorize @reminder
         if @reminder.save
             render :index, status: :created
+            puts "hello3"
         else
             render_error
+            puts "hello4"
         end
+      puts "hello5"
     end
 
     def destroy
@@ -42,8 +69,16 @@ class Api::V1::RemindersController < Api::V1::BaseController
 
 private
 
+     def check_time(string)
+      Chronic.parse ("f")
+
+      puts string
+      puts "hello6"
+      return string
+     end
+
      def reminder_params
-        params.require(:reminder).permit(:date, :day, :time, :recurrence, :content, :user_id)
+        params.require(:reminder).permit(:content)
      end
 
      def render_error
