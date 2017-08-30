a2c27b9b48fd168640fb37b3357bc5783ca6b4d8
valentin fage <valentin-fage@vfage.com>
2017-08-30 15:54:58 +0200

    final2

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Walt
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.active_record.time_zone_aware_types = [:datetime, :time]
-        # config.time_zone = "Paris"
-        # config.active_record.default_timezone = :local
+    # config.active_record.time_zone_aware_types = [:datetime, :time]
+        config.time_zone = "Paris"
+        config.active_record.default_timezone = :local
   end
 end
