13afea8cf7ead546eb998dac744d7a50ef35cf80
valentin fage <valentin-fage@vfage.com>
2017-08-31 18:52:26 +0200

    nouveau

--- db/migrate/20170831161921_remove_notifications_on_users.rb
+++ db/migrate/20170831161921_remove_notifications_on_users.rb
@@ -1,6 +1,6 @@
 class RemoveNotificationsOnUsers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :reminders, :web_notification, :boolean
-    remove_column :reminders, :phone_notification, :boolean
+    remove_column :users, :web_notification, :boolean
+    remove_column :users, :phone_notification, :boolean
   end
 end
