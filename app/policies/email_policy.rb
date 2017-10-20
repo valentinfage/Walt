c6b3e53576611bef4f9260959f553524726dea18
valentin fage <valentin-fage@vfage.com>
2017-10-20 14:55:03 +0200

    email commit

---
+++ app/policies/email_policy.rb
@@ -0,0 +1,20 @@
+class EmailPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user) #pour recuperer les reminders de l'user en cours de session
+      # scope.all # pour recuperer tous les reminders de tous les users.
+    end
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
